a11fa66d99bf4ba8f5ec9e2dc861218f79fb9040
yannlbh <yann.lebihan@edu.esiee.fr>
2023-06-21 21:52:38 +0200

    creation controller

--- pizzaApp/src/main/java/com/example/pizza/Application.java
+++ pizzaApp/src/main/java/com/example/pizza/Application.java
@@ -1,6 +1,5 @@
 package com.example.pizza;
 
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -9,10 +8,10 @@ import java.io.IOException;
 
 import java.sql.*;
 
-public class HelloApplication extends Application {
+public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Acceuil.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("Acceuil.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
@@ -20,12 +19,14 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
+        /*
         Connection cx = DBConnection.GetConn();
         Statement statement = cx.createStatement();
         ResultSet res = statement.executeQuery("SELECT * FROM pizza ;");
         while (res.next()) {
             System.out.println(res.getString("pizzaName"));
         }
+        */
 
         launch();
 

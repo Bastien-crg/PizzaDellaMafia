7e3c31e5a1e3cf7646b96279831cfffaa67ad664
Bastien <bastien.corgnac@gmail.com>
2023-06-23 21:07:51 +0200

    yabadabidou

--- pizzaApp/src/main/java/com/example/pizza/DBConnection.java
+++ pizzaApp/src/main/java/com/example/pizza/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     public static String DBurl = "jdbc:mysql://localhost:3306/rapizz";
     public static String user = "root";
-    public static String password = "";
+    public static String password = "root";
 
     public static Connection conn;
     public static Connection GetConn() throws SQLException {
--- pizzaApp/src/main/java/com/example/pizza/PizzaListeController.java
+++ pizzaApp/src/main/java/com/example/pizza/PizzaListeController.java
@@ -1,5 +1,7 @@
 package com.example.pizza;
 
+import com.example.pizza.model.pizza.Pizza;
+import com.example.pizza.model.pizza.PizzaDAO;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
@@ -16,6 +18,9 @@ import javafx.scene.text.Text;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class PizzaListeController implements Initializable {
@@ -31,8 +36,12 @@ public class PizzaListeController implements Initializable {
     @FXML
     private ListView<String> ListePizza;
 
-    String[] pizza = {"Reine","Calzonne","4 Fromages","Bastien t'es beau"};
+    List<Pizza> pizzas = PizzaDAO.getPizzas();
     String currentPizza;
+
+    public PizzaListeController() throws SQLException {
+    }
+
     @FXML
     void ButtonGoToAcceuil(MouseEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("Acceuil.fxml"));
@@ -44,7 +53,13 @@ public class PizzaListeController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        ListePizza.getItems().addAll(pizza);
+        ArrayList<String> pizzaStr = new ArrayList<>();
+        for (Pizza pizza :
+                pizzas) {
+            pizzaStr.add(pizza.toString());
+        }
+
+        ListePizza.getItems().addAll(pizzaStr);
 
         ListePizza.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
             @Override

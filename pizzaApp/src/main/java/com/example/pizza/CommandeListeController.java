e69351e5066e11cf88e1705ae84fadf003fbb53a
yannlbh <yann.lebihan@edu.esiee.fr>
2023-06-25 02:06:40 +0200

    liste commandes

--- pizzaApp/src/main/java/com/example/pizza/CommandeListeController.java
+++ pizzaApp/src/main/java/com/example/pizza/CommandeListeController.java
@@ -1,14 +1,16 @@
 package com.example.pizza;
 
-
 import com.example.pizza.model.client.ClientDAO;
-import com.example.pizza.model.commande.Commande;
 import com.example.pizza.model.commande.CommandDAO;
+import com.example.pizza.model.commande.Commande;
+import com.example.pizza.model.ingredient.Ingredient;
 import com.example.pizza.model.pizza.Pizza;
 import com.example.pizza.model.pizza.PizzaDAO;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
 import javafx.fxml.FXMLLoader;
@@ -17,6 +19,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
+import javafx.scene.text.Text;
 
 import java.io.IOException;
 import java.net.URL;
@@ -26,12 +29,14 @@ import java.util.List;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
-public class CommandeListeController {
+public class CommandeListeController implements Initializable {
     private Stage stage;
     private Scene scene;
     private Parent root;
     @FXML
     private Button Menu;
+    @FXML
+    private Text LabelCommande;
 
     @FXML
     private ListView<String> ListeCommande;
@@ -50,12 +55,16 @@ public class CommandeListeController {
         stage.show();
     }
 
-    public void initialize() throws SQLException {
+    public void initialize(URL url, ResourceBundle resourceBundle){
         ArrayList<String> commandeStr = new ArrayList<>();
-        System.out.println(commandes.toString());
+
         for (Commande commande :
                 commandes) {
-            commandeStr.add(ClientDAO.getCommandClient(commande.getIdCommande()).getNomClient() + " " + commande.getDateCommande().toString());
+            try {
+                commandeStr.add(getLabelText(commande));
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
 
         }
 
@@ -63,10 +72,35 @@ public class CommandeListeController {
 
         ListeCommande.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
             @Override
-            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
+            public void changed(ObservableValue<? extends String> observableValue, String s, String t1)  {
                 currentCommande = ListeCommande.getSelectionModel().getSelectedItem();
+                for(Commande commande :
+                        commandes){
+                    try {
+                        if(Objects.equals(currentCommande,getLabelText(commande))){
+
+                            String client = "Client : " + commande.getClient().getNomClient();
+                            String date = "Date : " + commande.getDateCommande().toString();
+                            List<Pizza> pizzaCommande = PizzaDAO.getCommandPizzas(commande.getIdCommande());
+                            String pizza = "Pizza : \n";
+                            for(Pizza piz : pizzaCommande){
+                                pizza += piz.getPizzaName().toString() + " , " + piz.getPrice() +"€\n";
+                            }
+                            String livreur = "Livreur : " +commande.getLivreur().getNomLivreur();
+                            String vehicule = "Vehicule : " + commande.getVehicule().getType();
+                            System.out.println(pizza);
+                            String label = client + "\n" + date + "\n" + pizza + "\n" + vehicule;
+                            LabelCommande.setText(label);
+                        }
+                    } catch (SQLException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
             }
         });
     }
 
+    private String getLabelText(Commande commande) throws SQLException {
+        return ClientDAO.getCommandClient(commande.getIdCommande()).getNomClient() + " " + commande.getDateCommande().toString() + " id :" + commande.getIdCommande();
+    }
 }
